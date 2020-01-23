3a4cb7adaf639bf4b06160b02e8b51be329c8769
Magami <magami@live2d.com>
2020-01-23 15:26:59 +0900

    fix MutationObserver

--- content_scripts.js
+++ content_scripts.js
@@ -10,10 +10,10 @@ const crys = [
 
 const imageCount = 37;
 
-function imgReplace() {
+function imgReplace () {
 
     const images = document.getElementsByClassName("css-1dbjc4n r-1niwhzg r-vvn4in r-u6sd8q r-4gszlv r-1p0dtai r-1pi2tsx r-1d2f490 r-u8s1d r-zchlnj r-ipm5af r-13qz1uu r-1wyyakw")
-    const tweets = document.getElementsByClassName("css-901oao css-16my406 r-gwet1z r-ad9z0x r-bcqeeo r-qvutc0");
+    const tweets = document.getElementsByClassName("css-901oao r-hkyrab r-gwet1z r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0");
     const hashTags = document.getElementsByClassName("css-4rbku5 css-18t94o4 css-901oao css-16my406 r-1n1174f r-1loqt21 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0")
 
     for (var i = 0; i < images.length; i++) {
@@ -35,17 +35,35 @@ function imgReplace() {
 
 }
 
-var observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-        imgReplace()
-    })
-});
+window.onload = () => {
 
-// オブザーバの設定
-const config = {
-    characterData: true,
-    subtree: true
-};
+  function initialObserver() {
 
-// 対象ノードとオブザーバの設定を渡す
-observer.observe(document, config);
+    const section = document.getElementsByTagName("section")[0];
+
+    if(!section){
+      setTimeout(initialObserver, 500);
+      return;
+    }
+  
+    // オブザーバの設定
+    const config = {
+      characterData: true,
+      childList: true,
+      subtree: true
+    };
+  
+    var observer = new MutationObserver(mutations => {
+  
+      observer.disconnect();
+      imgReplace();
+      observer.observe(section, config);
+    });
+  
+    // 対象ノードとオブザーバの設定を渡す
+    observer.observe(section, config);
+  }
+
+  initialObserver();
+
+}
